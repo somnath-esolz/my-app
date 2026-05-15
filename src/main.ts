e070361e2a9101df8581d5927be247ef0c09f473
somnath-esolz <somnath.sarkar@esolzmail.com>
2026-05-15 16:33:33 +0530

    add v1 to global prefix in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('v1');
+  app.setGlobalPrefix('v1', { exclude: ['/'] });
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
